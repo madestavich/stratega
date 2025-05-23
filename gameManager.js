88d16e83bf10e271137b83b83af0b8568e843eb1
Max Max <karlmadest@gmail.com>
2025-05-24 02:45:39 +0300

    123

--- gameManager.js
+++ gameManager.js
@@ -14,7 +14,7 @@ class GameManager {
   constructor() {
     this.lastTime = 0;
     this.deltaTime = 0;
-    this.fixedTimeStep = 1000 / 14;
+    this.fixedTimeStep = 800 / 14;
     this.accumulator = 0;
     this.debugMode = false;
     this.debugInterval = null;
