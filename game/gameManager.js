2c3aaca915cb08e61962d2a23693597cc7c58919
Max Max <karlmadest@gmail.com>
2025-08-13 16:51:18 +0300

    commit message

--- game/gameManager.js
+++ game/gameManager.js
@@ -427,6 +427,14 @@ class GameManager {
     console.log('Loading latest units from database...');
     await this.objectManager.loadObjects();
     
+    // Оновлюємо напрямок погляду після завантаження юнітів
+    for (const unit of this.objectManager.objects) {
+      unit.setLookDirectionByTeam();
+    }
+    for (const unit of this.objectManager.enemyObjects) {
+      unit.setLookDirectionByTeam();
+    }
+    
     // Save current player units and sync with enemy (ensures both players have same data)
     await this.objectManager.synchronizeAfterTurn();
     console.log('Units synchronized. Starting game...');
