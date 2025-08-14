41daf1c1ba0c8b8a3dbbfe04316a9dc9197c62f4
Max Max <karlmadest@gmail.com>
2025-08-14 19:36:45 +0300

    commit message

--- game_objects/gameObject.js
+++ game_objects/gameObject.js
@@ -155,17 +155,9 @@ export class GameObject {
     const isRoomCreator = gameManager ? gameManager.isRoomCreator : true;
 
     if (isRoomCreator) {
-      if (this.team === 1) {
-        this.lookDirection = { dx: 1, dy: 0 };
-      } else if (this.team === 2) {
-        this.lookDirection = { dx: -1, dy: 0 };
-      }
+      this.lookDirection = { dx: 1, dy: 0 };
     } else {
-      if (this.team === 1) {
-        this.lookDirection = { dx: -1, dy: 0 };
-      } else if (this.team === 2) {
-        this.lookDirection = { dx: 1, dy: 0 };
-      }
+      this.lookDirection = { dx: -1, dy: 0 };
     }
   }
 }
