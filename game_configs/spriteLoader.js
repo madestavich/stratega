4ec5fa0abfabb21ff1a739c3115ff97d109b821d
Max Max <karlmadest@gmail.com>
2025-08-25 19:12:02 +0300

    commit message

--- game_configs/spriteLoader.js
+++ game_configs/spriteLoader.js
@@ -11,6 +11,7 @@ export class SpriteLoader {
       zealot: "/game_configs/units/zealot.json",
       black_knight: "/game_configs/units/black_knight.json",
       bone_dragon: "/game_configs/units/bone_dragon.json",
+      lich: "/game_configs/units/lich.json",
       // Додайте інші спрайти тут
     };
   }
