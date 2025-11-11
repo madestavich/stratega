6700dc13eb1d711d26f733f9358d2b3e69663655
Max Max <karlmadest@gmail.com>
2025-11-11 19:33:57 +0200

    room lobby upd host check 3

--- server/auth/check_login.php
+++ server/auth/check_login.php
@@ -6,6 +6,7 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
     echo json_encode([
         'logged_in' => true,
         'user' => [
+            'id' => $_SESSION['user_id'],
             'username' => $_SESSION['username'],
             'email' => $_SESSION['email']
         ]
@@ -15,4 +16,3 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
         'logged_in' => false
     ]);
 }
-
